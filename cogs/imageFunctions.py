eb8aaec275894f17ce2fcb521d99e9afe5dd6b3f
Sprocket Tools <137387488+SprocketTools@users.noreply.github.com>
2025-10-17 17:29:06 -0700

    hi

--- cogs/imageFunctions.py
+++ cogs/imageFunctions.py
@@ -41,36 +41,44 @@ class imageTools(commands.Cog):
                 await ctx.send(f"Utility commands are restricted to the server's bot commands channel, but the server owner has not set a channel yet!  Ask them to run the `-setup` command in one of their private channels.")
                 return
 
-        await ctx.send("Send the url to your modifier image (either from sprockettools.github.io or Imgur), or reply with one of the following: `default preset`, `decal preset`, `dirt preset`, `vertical preset`, `seam preset`.")
-        def check(m: discord.Message):
-            return m.author.id == ctx.author.id and m.channel.id == ctx.channel.id
+        await ctx.send("How do you want to weather your decal?")
 
-        try:
-            msg = await self.bot.wait_for('message', check=check, timeout=200.0)
-            imageInput = str(msg.content).lower()
-            if imageInput == 'decal preset':
-                imageLink = 'https://sprockettools.github.io/textures/Scratch.png'
-            if imageInput == 'vertical preset':
-                imageLink = 'https://sprockettools.github.io/textures/Rusty_Metal.png'
-            if imageInput == 'seam preset':
-                imageLink = 'https://sprockettools.github.io/textures/Rusty_Metal_2.png'
-            if imageInput == 'dirt preset':
-                imageLink = 'https://sprockettools.github.io/img/weathering_overlay_alternative.png'
+
+        imageInput = await self.bot.ui.getButtonChoice(ctx, ["light scratches", "heavy scratches", "centerline wear", "water runoff", "dirt splotches", "mud stains", "custom"])
+
+
+        if imageInput == 'light scratches':
+            imageLink = 'https://sprockettools.github.io/img/black_scratches_1.png'
+        if imageInput == 'heavy scratches':
+            imageLink = 'https://sprockettools.github.io/img/white_scratches_2.png'
+        if imageInput == 'centerline wear':
+            imageLink = 'https://sprockettools.github.io/textures/Rusty_Metal.png'
+        if imageInput == 'water runoff':
+            imageLink = 'https://sprockettools.github.io/img/stain02.png'
+        if imageInput == 'mud stains':
+            imageLink = 'https://sprockettools.github.io/img/grey_scratches_2.png'
+        if imageInput == 'dirt splotches':
+            imageLink = 'https://sprockettools.github.io/img/weathering_overlay_alternative.png'
+        if imageInput == 'custom':
+            urlgram = await ctx.send("Send the url to your modifier image (either from https://sprockettools.github.io or Imgur)")
+            if "https://i.imgur.com" in imageInput or "https://sprockettools.github.io/" in imageInput:
+                imageLink = imageInput
             else:
-                if "https://i.imgur.com" in imageInput or "https://sprockettools.github.io/" in imageInput:
-                    imageLink = imageInput
+                await ctx.send("Invalid URL.  Reverting to default pattern...")
+                imageLink = 'https://sprockettools.github.io/img/white_scratches_2.png'
 
-
-        except asyncio.TimeoutError:
-            await ctx.reply("Weather command timed out.")
-            return
         await ctx.send("Beginning processing now.  This may take some time.")
 
         for item in attachments:
             # Brave AI actually works fairly decent
-            if int(item.size) > 25000000:
+            if int(item.size) > 20000000:
                 errorText = await self.bot.error.retrieveError(ctx)
-                await ctx.send(f"{errorText}\n\n{item.filename} is bigger than 25MB.  Please optimize this image a bit more to avoid issues when trying to use the decal.")
+                await ctx.send(f"{errorText}\n\n{item.filename} is bigger than 20MB.  Please optimize this image a bit more to avoid issues when trying to use the decal.")
+                return
+
+            if int(item.size) > 8000000:
+                errorText = await self.bot.error.retrieveError(ctx)
+                await ctx.send(f"{errorText}\n\n{item.filename} is bigger than 8MB.  I may be unable to reply with the image due to Discord filesize limitations.")
                 return
 
             response = requests.get(item.url)
@@ -83,7 +91,17 @@ class imageTools(commands.Cog):
             imageBaseR, imageBaseG,imageBaseB,imageBaseA = imageBase.split()
             imageModA = imageMod.split()[3]
 
-            newAlpha = ImageChops.subtract(imageBaseA, imageModA)
+            original_size = imageModA.size
+            width, height = original_size
+            left = int(width * 0.05)
+            top = int(height * 0.05)
+            right = int(width * 0.95)
+            bottom = int(height * 0.95)
+            crop_box = (left, top, right, bottom)
+            center_part = imageModA.crop(crop_box)
+            stretched_alpha_mask = center_part.resize(original_size, resample=Image.LANCZOS)
+
+            newAlpha = ImageChops.subtract(imageBaseA, stretched_alpha_mask)
             imageOut = Image.merge("RGBA", (imageBaseR, imageBaseG, imageBaseB, newAlpha))
 
             byte_io = io.BytesIO()
