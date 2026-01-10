15a3cb06a75d2a63791b06395d3690aa82294218
Sprocket Tools <137387488+SprocketTools@users.noreply.github.com>
2026-01-10 03:52:44 -0800

    Update githubTools.py

--- cogs/githubTools.py
+++ cogs/githubTools.py
@@ -28,7 +28,7 @@ except Exception as e:
 imageCategoryList = ["Featured", "Anime", "Chalk", "Fictional Insignia", "Flags", "Historical Insignia", "Inscriptions", "Labels",
                      "Letters", "Miscellaneous", "Memes", "Numbers", "Optics", "Seams", "Symbols", "Textures",
                      "Weathering", "Welding", "RGB Maker"]
-paintCategoryList = ["Featured", "WWI", "WWII", "Cold War", "Modern", "Fictional", "Memes"]
+paintCategoryList = ["Featured", "WWI", "WWII", "Cold War", "Modern", "Fictional", "Memes", "Other"]
 # GithubURL = "git@github.com:SprocketTools/SprocketTools.github.io.git"
 username = 'SprocketTools'
 password = main.githubPAT
