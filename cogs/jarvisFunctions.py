8a92b8bd81ade73e09a1d623e2761335bfb56581
Sprocket Tools <137387488+SprocketTools@users.noreply.github.com>
2026-09-01 00:25:54 -0700

    Update jarvisFunctions.py

--- cogs/jarvisFunctions.py
+++ cogs/jarvisFunctions.py
@@ -285,7 +285,8 @@ class jarvisFunctions(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
-
+        if not self.bot.ui.channel_block_check(message.channel.id, "ai"):
+            return
         if not self.bot.operational and (message.author.id != self.bot.owner_id):
             return
         print("AA")
